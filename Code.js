a746729e4b9dca12074b89a529e51ba04b251d2c
JusticeTemple <77866705+JusticeTemple@users.noreply.github.com>
2021-07-30 19:21:28 -0700

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+fill("white")
+textSize(15)
+text("N U T O F I C N", 700,140)
+fill("lightblue")
+text("Hint: Performs a particular task !!", 700,170)
+
+fill("white")
+textSize(15)
+text("A T E D A B S A", 333,290)
+fill("lightblue")
+text("Hint: Stores all information !!", 333,310)
+
+
+//text(mouseX + "," + mouseY,mouseX,mouseY);
+}
